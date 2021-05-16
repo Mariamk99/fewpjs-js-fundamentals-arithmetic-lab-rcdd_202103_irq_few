834e912a2bfe8d34bb9fcf901aa59d670ec15405
Mariam  Khamo <mariamw736@gmail.com>
2021-05-16 21:27:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ let num1 = 2;
 let num2 = 31;
 let multiply = num1 * num2; 
 
+<<<<<<< HEAD
 let random = Math.floor(Math.random() * 11);
 console.log(random);
 
@@ -12,3 +13,5 @@ var num3 = 19;
 var num4 = 5;
 var mod = num3 % num4;
 var max = Math.max(5, 16, 18, 20);
+=======
+>>>>>>> 96b0bce40d1fd326953f58a7fb2fb0a73777e835
